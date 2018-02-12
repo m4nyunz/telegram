73273c98dc33bab262781ec0beb4d4a5b728c4e4
m4nyunz <31151067+m4nyunz@users.noreply.github.com>
2018-02-12 07:19:59 +0700

    Update m4nyunzbot.php

--- m4nyunzbot.php
+++ m4nyunzbot.php
@@ -6,44 +6,40 @@ $userendpoint = "$teleurl$usertokenbot/";
 
 $messages = $Hook["params"];
 if (isset($messages['message'])) {
-  $messages = $messages['message'];
-} else {
-  echo 'URL hook.io: https://'.$Hook['input']['host'].$Hook['input']['path'];
-}
+  $messages = $messages['message'];} else {
+  echo 'URL hook.io: https://'.$Hook['input']['host'].$Hook['input']['path'];}
+
+// memberikan informasi ID
+if (isset($messages['text'])="/id") {
+  reply_to_message
+  $pesan = "Your ID is ".$messages['chat']['id']."!\n";
+  kirimPesan($messages['chat']['id'], $pesan);}
+
 
 // ngucapin selamat datang member baru
 if (isset($messages['new_chat_member'])) {
   $pesan = "Hai ".$messages['new_chat_member']['first_name']."!\n";
   $pesan.= "Selamat datang di Grup ".$messages['chat']['title'];
-  kirimPesan($messages['chat']['id'], $pesan);
-}
+  kirimPesan($messages['chat']['id'], $pesan);}
 
 // ngucapin selamat tinggal buat user yang pergi
 // HANYA jika telegram mengirimkan signal left member
 if (isset($messages['left_chat_member'])) {
   $pesan = "Sampai jumpa lagi ".$messages['left_chat_member']['first_name'] ;
-  kirimPesan($messages['chat']['id'], $pesan);
-} 
+  kirimPesan($messages['chat']['id'], $pesan);} 
 
 // fungsi Kirim Pesan
-function kirimPesan($chat_id, $text)
-{
+function kirimPesan($chat_id, $text){
     global $endpoint;
     $data = array(
         'chat_id' => $chat_id,
-        'text'  => $text
-    );
- 
+        'text'  => $text); 
   	// Buat parameter pengiriman
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
             'method'  => 'POST',
-            'content' => http_build_query($data),
-        ),
-    );
+            'content' => http_build_query($data),),);
     $context  = stream_context_create($options); 
     $result = file_get_contents($endpoint.'sendMessage', false, $context);
-   
-    return $result;
-}
+    return $result;}
